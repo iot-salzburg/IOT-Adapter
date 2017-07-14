1701698123c61d29e776a26db477defcd1dca44f
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2017-07-14 13:44:39 +0200

    used metric-transformation

--- adapter.py
+++ adapter.py
@@ -28,88 +28,110 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def forward_message():
+def forward():
     for msg in consumer:
+        data = json.dumps(msg)
+        var = str(msg).split('value={')[1].split('}')[0] + ","
+        print(var.count(","), msg)
 
         datapoint = fetch_sensor_data(msg)
         if datapoint is None:
             continue
 
-        print("Datapoint: {}".format(datapoint))
-
         message = convert_to_sensorthings(datapoint)
         publish_message(message)
-
-        time.sleep(0.4)
 
 
 def fetch_sensor_data(msg):
     try:
-        datapoint = dict()
-        structure = str(msg).count("{")
+        data = json.dumps(msg)
+        if "airquality" in data:
+            string = str(msg).split("'data':")[1].split('}')[0] + ","
 
-        if "rasp02/um2-pos" in str(msg):
+            value_in = string.split("'level':")[1].split(',')[0]
+            value_in = float(re.sub('[^.0-9]+', '', value_in))
+
+            ts_in = string.split("'ts':")[1]
+            ts_in = float(re.sub('[^0-9]+', '', ts_in))
+
+            topic_in = str(msg).split("topic':")[1].split(",")[0]
+            topic_in = topic_in.replace("'", "")
+
+        elif "rasp06/temp" in data:
+            var = str(msg).split('value={')[1].split('}')[0] + ","
+
+            value_in = var.split("'data':")[1].split("'temp':")[1].split(",")[0]
+            value_in = float(re.sub('[^.0-9]+', '', value_in))
+
+            ts_in = var.split("'ts':")[1].split(",")[0]
+            ts_in = float(re.sub('[^0-9]+', '', ts_in)[:13])
+
+            topic_in = str(msg).split("topic':")[1].split("}")[0].split(",")[0]
+            topic_in = topic_in.replace("'", "")
+
+        elif "rasp02/um2-pos" in data:
             var = str(msg).split('value={')[1].split('}')[0] + ","
             if "posX" in var and "posZ" in var:
                 return None
             for pos in ["posX", "posY", "posZ", "posE"]:
                 if pos in var:
                     value_in = var.split("'data':")[1].split(pos)[1].split(",")[0]
-                    datapoint["value"] = float(re.sub('[^.0-9]+', '', value_in))
-                    datapoint["quantity"] = "rasp02/um2-" + pos
-            ts_in = var.split("'ts':")[1].split(",")[0]
-            datapoint["ts"] = float(re.sub('[^0-9]+', '', ts_in)[:13])
+                    value_in = float(re.sub('[^.0-9]+', '', value_in))
+                    topic_in = "rasp02/um2-" + pos
 
-        elif "rasp02/um2-temp" in str(msg):
+            ts_in = var.split("'ts':")[1].split(",")[0]
+            ts_in = float(re.sub('[^0-9]+', '', ts_in)[:13])
+
+        elif "rasp02/um2-temp" in data:
             var = str(msg).split('value={')[1].split('}')[0] + ","
             if "targetNozzle" in var and "tempBed" in var:
                 return None
             for temp in ["targetNozzle", "targetBed", "tempNozzle", "tempBed"]:
                 if temp in var:
                     value_in = var.split("'data':")[1].split(temp)[1].split(",")[0]
-                    datapoint["value"] = float(re.sub('[^.0-9]+', '', value_in))
-                    datapoint["quantity"] = "rasp02/um2-" + temp
+                    value_in = float(re.sub('[^.0-9]+', '', value_in))
+                    topic_in = "rasp02/um2-" + temp
+
             ts_in = var.split("'ts':")[1].split(",")[0]
-            datapoint["ts"] = float(re.sub('[^0-9]+', '', ts_in)[:13])
+            ts_in = float(re.sub('[^0-9]+', '', ts_in)[:13])
 
-        if structure == 1:
-            var = str(msg).split('{')[1].split('}')[0] + ","
-            value_in = var.split("data")[1].split(",")[0]
-            datapoint["value"] = float(re.sub('[^.0-9]+', '', value_in))
-            ts_in = var.split("ts")[1].split(",")[0]
-            datapoint["ts"] = float(re.sub('[^.0-9]+', '', ts_in))
 
-            topic_in = var.split("topic")[1].split(":")[1].split(",")[0]
-            datapoint["quantity"] = topic_in.replace("'", "").replace('"','').strip()
+        elif "filaConsumption" in data:
+            var = str(msg).split('value={')[1].split('}')[0] + ","
 
-        elif structure == 2:
-            inner = str(msg).split('{')[2].split("}")[0].split(",")
-            for metric in inner:
-                if "'ts':" in metric:
-                    datapoint["ts"] = float(re.sub('[^.0-9]+', '', metric))
-                else:
-                    datapoint["value"] = float(re.sub('[^.0-9]+', '', metric))
+            value_in = var.split("'data':")[1].split("'filaConsumption':")[1].split(",")[0]
+            value_in = float(re.sub('[^.0-9]+', '', value_in))
 
-            var = "".join("".join(str(msg).split('{')[1:]).split("}")[:-1])
-            topic_in = var.split("topic")[1].split(":")[1].split(",")[0]
-            datapoint["quantity"] = topic_in.replace("'", "").replace('"', '').strip()
+            ts_in = var.split("'ts':")[1].split(",")[0]
+            ts_in = float(re.sub('[^0-9]+', '', ts_in)[:13])
+
+            topic_in = str(msg).split("topic':")[1].split("}")[0].split(",")[0]
+            topic_in = topic_in.replace("'", "")
 
         else:
-            print("\nFetched undefined input:")
             var = str(msg).split('value={')[1].split('}')[0] + ","
+
             value_in = var.split("data':")[1].split(",")[0]
-            datapoint["value"] = float(re.sub('[^.0-9]+', '', value_in))
+            value_in = float(re.sub('[^.0-9]+', '', value_in))
+
             ts_in = var.split("ts':")[1].split(",")[0]
-            datapoint["ts"] = float(re.sub('[^.0-9]+', '', ts_in))
+            ts_in = float(re.sub('[^.0-9]+', '', ts_in))
 
             topic_in = str(msg).split('value={')[1].split("topic':")[1].split(",")[0]
-            datapoint["quantity"] = topic_in.replace("'", "")
+            topic_in = topic_in.replace("'", "")
+
+        time.sleep(0.5)
+        # print("ts: {}, \tdata: {},      \ttopic: {}".format(ts_in, value_in, topic_in))
 
     except:
-        print("FetchingExceptionRaised:\n")
+        print("ExceptionRaised:\n")
         print(msg)
         sys.exit()
 
+    datapoint = dict()
+    datapoint["quantity"] = topic_in
+    datapoint["ts"] = ts_in
+    datapoint["value"] = value_in
     return datapoint
 
 
@@ -141,11 +163,10 @@ def transform_timestamp(ts_in, form="ISO8601"):
         return observation_time
 
 
-
 def publish_message(message):
     producer.send(KAFKA_TOPIC_OUT, message)
     pass
 
 
 if __name__ == '__main__':
-    forward_message()
+    forward()
